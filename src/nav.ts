c7d1df11411d1446560e3802166ac13e1c97e5d0
Agus Setiawan <agussetiawan.me02@gmail.com>
2023-09-21 14:11:34 +0700

    first commit

---
+++ src/nav.ts
@@ -0,0 +1,18 @@
+export const nav = [
+  {
+    title: "Projects",
+    slug: "#projects",
+  },
+  {
+    title: "Work Experiences",
+    slug: "#work-experiences",
+  },
+  {
+    title: "Contact Me",
+    slug: "#contact-me",
+  },
+  {
+    title: "✨Blog",
+    slug: "/blog",
+  },
+];
